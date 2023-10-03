ebdd6bedd663214804017b8b5b7a19a6e44daf32
rodrigo4ka <rori.4k@gmail.com>
2023-10-03 18:47:35 -0400

    if

---
+++ if.py
@@ -0,0 +1,16 @@
+estatura = float(input("ingrese su estatura"))
+
+if estatura >= 2.00:
+    print("eres humano?")
+elif estatura >= 1.90:
+    print("eres una jirafa")
+elif estatura >= 1.80:
+    print("eres alto")
+elif estatura >= 1.70:
+    print("ok normal")
+elif estatura >= 1.60:
+    print("eres enano")
+elif estatura >= 1.50:
+    print("eres un pitufo")
+else:
+    print("hormiga")
